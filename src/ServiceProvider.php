cf2c248bc2ccaf1d8e805799755fa285b79090d2
Ohtarr <ohtarr@gmail.com>
2022-09-28 15:50:40 -0500

    Fixing publish

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -18,7 +18,7 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         $this->installOauthTokenGuardMiddleware();
 
         $this->publishes([
-            __DIR__.'/../publish/2022_09_27_200151_alter_users_table_for_azure_ad.php'                     => $this->app->databasePath().'/migrations/2022_09_27_200151_alter_users_table_for_azure_ad.php',
+            __DIR__.'/publish/2022_09_27_200151_alter_users_table_for_azure_ad.php'                     => $this->app->databasePath().'/migrations/2022_09_27_200151_alter_users_table_for_azure_ad.php',
         ]);
 
     }
